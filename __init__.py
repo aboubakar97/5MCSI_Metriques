2d4ec7699541cfc55b3fcee8b8aff97f326578e7
aboubakar97 <aboubakarpp@gmail.com>
2025-06-16 12:14:03 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -58,6 +58,12 @@ def commits_minute_distribution():
 
     return jsonify(results=results)
 
+@app.route('/extract-minutes/<date_string>')
+def extract_minutes(date_string):
+    date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
+    minutes = date_object.minute
+    return jsonify({'minutes': minutes})
+
 @app.route("/commits/")
 def commits():
     return render_template("commits.html")
